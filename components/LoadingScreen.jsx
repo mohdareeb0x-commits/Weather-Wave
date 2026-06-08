b69901ef714b936012725dbaac77187d3a1f2c13
Mohd Areeb <mohdareeb0x@gmail.com>
2026-06-09 01:07:58 +0530

    Small bugs resolved

--- components/LoadingScreen.jsx
+++ components/LoadingScreen.jsx
@@ -12,19 +12,57 @@ const LoadingScreen = () => {
         className="absolute w-full h-full z-0"
       />
       <View className="h-60 w-full mt-5 items-center justify-around">
-        <Text className="color-white text-2xl font-bold font-poppins">Loading...</Text>
+        <Text className="color-white text-2xl font-bold font-poppins">
+          Loading...
+        </Text>
       </View>
       <View className="flex-row self-center justify-between w-10/12">
-        <SmallCard />
-        <SmallCard />
+        <SmallCard
+          icon={null}
+          value=""
+          unit=""
+          size=""
+          text=""
+        />
+        <SmallCard
+          icon={null}
+          value=""
+          unit=""
+          size=""
+          text=""
+        />
       </View>
       <View className="flex-row self-center justify-between w-10/12">
-        <SmallCard />
-        <SmallCard />
+        <SmallCard
+          icon={null}
+          value=""
+          unit=""
+          size=""
+          text=""
+        />
+        <SmallCard
+          icon={null}
+          value=""
+          unit=""
+          size=""
+          text=""
+        />
       </View>
       <View className="flex-row self-center justify-between w-10/12">
-        <SmallCard />
-        <SmallCard />
+        <SmallCard
+          icon={null}
+          value=""
+          unit=""
+          size=""
+          text=""
+        />
+        <SmallCard
+          icon={null}
+          value=""
+          unit=""
+          size=""
+          text=""
+        />
       </View>
     </SafeAreaView>
   );
