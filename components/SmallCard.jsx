009a648a4a5a2e9e78cba424d1576c6049747921
Mohd Areeb <mohdareeb0x@gmail.com>
2026-06-09 01:23:40 +0530

    Small bugs resolved

--- components/SmallCard.jsx
+++ components/SmallCard.jsx
@@ -1,11 +1,18 @@
 import { Image, Text, View } from "react-native";
 
 const SmallCard = ({ icon, text, value, unit, size }) => {
+  if (!icon) {
+    return (
+      <View className="h-28 w-40 bg-white/20 items-center p-3 mt-5 border-4 border-t-0 border-b-8 border-black/20 rounded-3xl  justify-around"></View>
+    );
+  }
   return (
     <View className="h-28 w-40 bg-white/20 items-center p-3 mt-5 border-4 border-t-0 border-b-8 border-black/20 rounded-3xl  justify-around">
       <Image source={icon} className={size} tintColor="white" />
       <Text className="font-poppins color-white/40 mt-2 text-sm">{text}</Text>
-      <Text className="font-poppins color-white justify-self-end">{value} {unit}</Text>
+      <Text className="font-poppins color-white justify-self-end">
+        {value} {unit}
+      </Text>
     </View>
   );
 };
